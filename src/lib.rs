f9de1dd5a06bc6c9b2d1565b34894de405159f65
IceBlockProduction <iceblockproduction@protonmail.com>
2022-07-20 18:18:23 +0200

    SUNDAE padding works for AD

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use core::arch::x86_64 as arch;
 
 use aead::Payload;
 use arch::{
-    __m128i, _mm_loadu_si128, _mm_set_epi8, _mm_storeu_si128, _mm_xor_si128, _mm_shuffle_epi8
+    __m128i, _mm_loadu_si128, _mm_set_epi8, _mm_shuffle_epi8, _mm_storeu_si128, _mm_xor_si128,
 };
 
 pub use aead::{self, AeadCore, AeadInPlace, Error, NewAead};
@@ -18,7 +18,7 @@ pub use cipher::Key;
 use cipher::{
     consts::{U0, U16, U8},
     generic_array::{ArrayLength, GenericArray},
-    BlockCipher, BlockDecrypt, BlockEncrypt, BlockSizeUser, KeyInit, KeySizeUser,
+    BlockCipher, BlockEncrypt, BlockSizeUser, KeyInit, KeySizeUser,
 };
 
 use core::marker::PhantomData;
@@ -54,7 +54,7 @@ where
 
 impl<B, NonceSize> NewAead for Sundae<B, NonceSize>
 where
-    B: BlockSizeUser<BlockSize = U16> + BlockEncrypt + BlockDecrypt + KeyInit,
+    B: BlockSizeUser<BlockSize = U16> + BlockEncrypt + KeyInit,
 {
     type KeySize = B::KeySize;
 
@@ -65,7 +65,7 @@ where
 
 impl<B, NonceSize> From<B> for Sundae<B, NonceSize>
 where
-    B: BlockSizeUser<BlockSize = U16> + BlockEncrypt + BlockDecrypt,
+    B: BlockSizeUser<BlockSize = U16> + BlockEncrypt,
 {
     fn from(cipher: B) -> Self {
         Self {
@@ -86,12 +86,12 @@ where
 
 impl<B, NonceSize> AeadInPlace for Sundae<B, NonceSize>
 where
-    B: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockEncrypt + BlockDecrypt,
+    B: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockEncrypt,
     NonceSize: ArrayLength<u8>,
 {
     fn encrypt_in_place_detached(
         &self,
-        nonce: &Nonce<NonceSize>,
+        _nonce: &Nonce<NonceSize>,
         associated_data: &[u8],
         buffer: &mut [u8],
     ) -> Result<Tag, Error> {
@@ -103,60 +103,102 @@ where
             // Setting the initial value for whether pt is empty or not
             let b2: i8 = if pt_len > 0 { 0x4 } else { 0 };
 
-            let mul2 = _mm_set_epi8(14,13,12,11,10,9,8,7,6,5,4,3,2,1,0,-1);
-            let xor2 = _mm_set_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, 15, -1, 15, -1);
+            let mul2 = _mm_set_epi8(14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1);
+            let _xor2 = _mm_set_epi8(
+                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, 15, -1, 15, -1,
+            );
+
+            let mut buf = [0u8; 16];
 
             let mut block: __m128i;
-            let mut v = self.bc_encrypt(_mm_set_epi8((b1 | b2) << 4,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0));
-            
-            let mut tag = [0u8; 16];
+            let mut v = self.bc_encrypt(_mm_set_epi8(
+                (b1 | b2) << 4,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+            ));
+
+            let tag = [0u8; 16];
             _mm_storeu_si128(tag[..].as_ptr() as *mut __m128i, v);
 
             let mut block_start = 0;
             let mut block_end = 16;
 
             // Tag computing over associated data
-            if ad_len > 0 {
-                while ad_len > 16 {
-                    block = _mm_loadu_si128(associated_data[block_start..block_end].as_ptr() as *const __m128i);
-                    v = self.bc_encrypt(_mm_xor_si128(v, block));
-    
-                    ad_len -= 16;
-                    block_start += 16;
-                    block_end += 16;
-                }
-                block = _mm_loadu_si128(associated_data[block_start..].as_ptr() as *const __m128i);
-                v = _mm_shuffle_epi8(mul2, _mm_xor_si128(v, block));
-                v = self.bc_encrypt(v);
-                _mm_storeu_si128(tag[..].as_ptr() as *mut __m128i, v);
+            while ad_len > 16 {
+                block = _mm_loadu_si128(
+                    associated_data[block_start..block_end].as_ptr() as *const __m128i
+                );
+                v = self.bc_encrypt(_mm_xor_si128(v, block));
+
+                ad_len -= 16;
+                block_start += 16;
+                block_end += 16;
             }
+            // Copy remaining bytes from associated data
+            buf[..ad_len].copy_from_slice(&associated_data[block_start..]);
+            
+            // If remaining block of ad is incomplete pad it
+            if ad_len < 16 {
+                buf[ad_len] = 0x80;
+            }
+
+            block = _mm_loadu_si128(buf[..].as_ptr() as *const __m128i);
+            v = _mm_shuffle_epi8(mul2, _mm_xor_si128(v, block));
+            v = self.bc_encrypt(v);
+            _mm_storeu_si128(tag[..].as_ptr() as *mut __m128i, v);
 
             block_start = 0;
             block_end = 16;
 
-            if pt_len > 0 {
-                while pt_len > 16 {
-                    block = _mm_loadu_si128(buffer[block_start..block_end].as_ptr() as *const __m128i);
-                    v = self.bc_encrypt(_mm_xor_si128(v, block));
-    
-                    pt_len -= 16;
-                    block_start += 16;
-                    block_end += 16;
-                }
+            // Tag computing over plaintext
+            while pt_len > 16 {
                 block = _mm_loadu_si128(buffer[block_start..block_end].as_ptr() as *const __m128i);
-                v = _mm_shuffle_epi8(mul2, _mm_xor_si128(v, block));
-                v = self.bc_encrypt(v);
-                _mm_storeu_si128(tag[..].as_ptr() as *mut __m128i, v);
+                v = self.bc_encrypt(_mm_xor_si128(v, block));
+
+                pt_len -= 16;
+                block_start += 16;
+                block_end += 16;
             }
+            buf = [0u8; 16];
+            buf[..pt_len].copy_from_slice(&buffer[block_start..]);
+
+            if pt_len < 16 {
+                buf[pt_len] = 0x80;
+            }
+
+            block = _mm_loadu_si128(buf[..].as_ptr() as *const __m128i);
+            v = _mm_shuffle_epi8(mul2, _mm_xor_si128(v, block));
+            v = self.bc_encrypt(v);
+            _mm_storeu_si128(tag[..].as_ptr() as *mut __m128i, v);
 
             block_start = 0;
             block_end = 16;
             pt_len = buffer.len();
 
+            // Actual encryption procedure
             while pt_len > 0 {
                 v = self.bc_encrypt(v);
 
-                _mm_storeu_si128(buffer[block_start..block_end].as_ptr() as *mut __m128i, _mm_xor_si128(_mm_loadu_si128(buffer[block_start..block_end].as_ptr() as *const __m128i), v));
+                _mm_storeu_si128(
+                    buffer[block_start..block_end].as_ptr() as *mut __m128i,
+                    _mm_xor_si128(
+                        _mm_loadu_si128(buffer[block_start..block_end].as_ptr() as *const __m128i),
+                        v,
+                    ),
+                );
 
                 block_start += 16;
                 block_end += 16;
@@ -182,24 +224,27 @@ where
             let mut block: __m128i;
             let mut v = _mm_loadu_si128(tag[..].as_ptr() as *const __m128i);
 
+            // Decryption procedure
             while ct_len > 0 {
                 v = self.bc_encrypt(v);
                 block = _mm_loadu_si128(buffer[block_start..block_end].as_ptr() as *const __m128i);
-                _mm_storeu_si128(buffer[block_start..block_end].as_ptr() as *mut __m128i, _mm_xor_si128(v, block));
-                
+                _mm_storeu_si128(
+                    buffer[block_start..block_end].as_ptr() as *mut __m128i,
+                    _mm_xor_si128(v, block),
+                );
+
                 block_start += 16;
                 block_end += 16;
                 ct_len -= 16;
             }
 
-            if !buffer.is_empty() {
-                let payload = Payload {
-                    aad: associated_data,
-                    msg: buffer
-                };
-                let test = self::aead::Aead::encrypt(self, nonce, payload).expect("Encryption error");
-                assert!(test[test.len()-16..] == tag.to_vec());
-            }
+            // Tag verification
+            let payload = Payload {
+                aad: associated_data,
+                msg: buffer,
+            };
+            let test = self::aead::Aead::encrypt(self, nonce, payload).expect("Encryption error");
+            assert!(test[test.len() - 16..] == tag.to_vec());
 
             Ok(())
         }
@@ -208,7 +253,7 @@ where
 
 impl<B, NonceSize> Sundae<B, NonceSize>
 where
-    B: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockEncrypt + BlockDecrypt,
+    B: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockEncrypt,
     NonceSize: ArrayLength<u8>,
 {
     // Encryption procedure of the internal block cipher
@@ -216,14 +261,6 @@ where
     fn bc_encrypt(&self, _in: __m128i) -> __m128i {
         let mut tmp = u8x16::from(_in);
         self.cipher.encrypt_block(tmp.as_mut_array().into());
-        tmp.into()
-    }
-
-    // Decryption procedure of the internal block cipher
-    #[inline]
-    fn bc_decrypt(&self, _in: __m128i) -> __m128i {
-        let mut tmp = u8x16::from(_in);
-        self.cipher.decrypt_block(tmp.as_mut_array().into());
         tmp.into()
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,3 +1,4 @@
+use aead::Payload;
 use sundae::{aead::Aead, NewAead, SundaeAes};
 
 #[test]
@@ -5,9 +6,37 @@ fn test_encrypt() {
     let key = [0u8; 16];
     let cipher = SundaeAes::new(&key.into());
 
-    let res = cipher.encrypt(&[0u8; 8].into(), [0u8; 128].as_ref()).expect("Encryption failed");
+    let res = cipher
+        .encrypt(&[0u8; 8].into(), [0u8; 16].as_ref())
+        .expect("Encryption failed");
 
     println!("{:#02x?}", res);
+}
+
+#[test]
+fn test_ad() {
+    let key = [0u8; 16];
+    let cipher = SundaeAes::new(&key.into());
+
+    let payload = Payload {
+        aad: &[0u8; 16],
+        msg: &[0u8; 0],
+    };
+
+    let enc = cipher
+        .encrypt(&[0u8; 8].into(), payload)
+        .expect("Encryption failed");
+
+    println!("{:#02x?}", enc);
+
+    let payload = Payload {
+        aad: &[0u8; 16],
+        msg: &enc,
+    };
+
+    let dec = cipher.decrypt(&[0u8; 8].into(), payload).expect("Decryption failed");
+
+    println!("{:#02x?}", dec);
 }
 
 #[test]
@@ -15,11 +44,43 @@ fn test_decrypt() {
     let key = [0u8; 16];
     let cipher = SundaeAes::new(&key.into());
 
-    let enc = cipher.encrypt(&[0u8; 8].into(), [0u8; 16].as_ref()).expect("Encryption failed");
+    let enc = cipher
+        .encrypt(&[0u8; 8].into(), [0u8; 16].as_ref())
+        .expect("Encryption failed");
 
     println!("{:#02x?}", enc);
 
-    let dec = cipher.decrypt(&[0u8; 8].into(), enc.as_ref()).expect("Decryption error");
+    let dec = cipher
+        .decrypt(&[0u8; 8].into(), enc.as_ref())
+        .expect("Decryption error");
+
+    println!("{:#02x?}", dec);
+
+    assert_eq!([0u8; 16], dec.as_ref());
+}
+
+#[test]
+fn pt_and_ad() {
+    let key = [0u8; 16];
+    let cipher = SundaeAes::new(&key.into());
+
+    let payload = Payload {
+        aad: &[0u8; 16],
+        msg: &[1u8; 64],
+    };
+
+    let enc = cipher
+        .encrypt(&[0u8; 8].into(), payload)
+        .expect("Encryption failed");
+
+    println!("{:#02x?}", enc);
+
+    let payload = Payload {
+        aad: &[0u8; 16],
+        msg: &enc,
+    };
+
+    let dec = cipher.decrypt(&[0u8; 8].into(), payload).expect("Decryption failed");
 
     println!("{:#02x?}", dec);
 }
@@ -29,8 +90,10 @@ fn sanity_check() {
     let key = [0u8; 16];
     let cipher = SundaeAes::new(&key.into());
 
-    let res = cipher.encrypt(&[0u8; 8].into(), [0u8; 128].as_ref());
-    res.expect("Encryption failed");
-    let res = cipher.decrypt(&[0u8; 8].into(), [0u8; 128].as_ref());
-    res.expect("Decryption failed");
+    let res = cipher
+        .encrypt(&[0u8; 8].into(), [0u8; 128].as_ref())
+        .expect("Encryption failure");
+    cipher
+        .decrypt(&[0u8; 8].into(), res.as_ref())
+        .expect("Decryption failure");
 }
